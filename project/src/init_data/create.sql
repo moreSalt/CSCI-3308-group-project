46ec7bba2060ad19d87a85d4cb082bc8bd639f79
Jared Preyer <japr8735@colorado.edu>
2023-11-10 11:52:22 -0700

    UPDATE DATABASE

--- project/src/init_data/create.sql
+++ project/src/init_data/create.sql
@@ -1,11 +1,13 @@
+DROP TABLE IF EXISTS users;
 -- For Login in Page and Creating a user
 CREATE TABLE users(
-    username VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(20) NOT NULL,
 );
+DROP TABLE IF EXISTS reviews;
 -- For Reviews have users_id with the text, rating, and date
 CREATE TABLE reviews(
     review TEXT NOT NULL,
     rating FLOAT NOT NULL,
     date DATE NOT NULL,
-);
+);;
