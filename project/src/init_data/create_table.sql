f028dbe0e9fa1d3a7ccf590193b5511b52b3c233
Jared Preyer <japr8735@colorado.edu>
2023-11-10 10:44:43 -0700

    Views file updated

--- project/src/init_data/create_table.sql
+++ project/src/init_data/create_table.sql
@@ -1,8 +1,10 @@
-create TABLE users(
+-- For Login in Page and Creating a user
+CREATE TABLE users(
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
     users_id INT AUTO_INCREMENT PRIMARY KEY
 );
+-- For Reviews have users_id with the text, rating, and date
 CREATE TABLE reviews(
     users_id INT NOT NULL,
     FOREIGN KEY (users_id) REFERENCES users(users_id),
