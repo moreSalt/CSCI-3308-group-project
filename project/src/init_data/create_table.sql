8039c4d8f7a4691299f69e23178dd052aad15ce0
Jared Preyer <japr8735@colorado.edu>
2023-11-10 11:45:53 -0700

    UPDATE DATABASE

--- project/src/init_data/create_table.sql
+++ project/src/init_data/create_table.sql
@@ -2,13 +2,9 @@
 CREATE TABLE users(
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
-    users_id INT NOT NULL AUTO_INCREMENT
-    PRIMARY KEY (users_id)
 );
 -- For Reviews have users_id with the text, rating, and date
 CREATE TABLE reviews(
-    users_id INT NOT NULL,
-    FOREIGN KEY (users_id) REFERENCES users(users_id),
     review TEXT NOT NULL,
     rating FLOAT NOT NULL,
     date DATE NOT NULL,
