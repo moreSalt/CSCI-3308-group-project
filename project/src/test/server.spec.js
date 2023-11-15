08ea7858f0703166d27ff5e032bfd895612fc28a
Alexis Mollet <almo8274@colorado.edu>
2023-11-15 08:58:42 -0700

    test/server.spec.js: Mocha/Chai outline and milestones/UAT_testing document complete

--- project/src/test/server.spec.js
+++ project/src/test/server.spec.js
@@ -8,7 +8,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.should();
 chai.use(chaiHttp);
-const {assert, expect} = chai;
+const { assert, expect } = chai;
 
 
 describe('Server!', () => {
@@ -29,5 +29,31 @@ describe('Server!', () => {
   // ===========================================================================
   // TO-DO: Part A Login unit test case
 
-  
+  //We are checking POST /add_user API by passing the user info in the correct order. This test case should pass and return a status 200 along with a "Success" message.
+  //Positive cases
+  it('positive : /add_user', done => {
+    chai
+      .request(server)
+      .post('/add_user')
+      .send({ id: 5, name: 'John Doe', dob: '2020-02-20' })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals('Success');
+        done();
+      });
+  });
+
+  //We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 200 along with a "Invalid input" message.
+  it('Negative : /add_user. Checking invalid name', done => {
+    chai
+      .request(server)
+      .post('/add_user')
+      .send({ id: '5', name: 10, dob: '2020-02-20' })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals('Invalid input');
+        done();
+      });
+  });
+
 });
