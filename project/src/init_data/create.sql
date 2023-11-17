9c5b3903c212b4d84e36487bc7ad115a6645c324
Jared Preyer <japr8735@colorado.edu>
2023-11-17 10:52:55 -0700

    Created Photos, Comments, Likes DB

--- project/src/init_data/create.sql
+++ project/src/init_data/create.sql
@@ -1,15 +1,34 @@
 -- using CREATE TABLE IF NOT EXISTS:l ensures that existing data is not lost due to the table being dropped and recreated
 -- For Login in Page and Creating a user
 CREATE TABLE IF NOT EXISTS users(
-    username VARCHAR(50) PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(70) NOT NULL
 );
--- CREATE TABLE IF NOT EXISTS reviews;
--- For Reviews have users_id with the text, rating, and date
-CREATE TABLE IF NOT EXISTS reviews(
-    review TEXT NOT NULL,
-    rating FLOAT NOT NULL,
-    review_date DATE NOT NULL
+-- Photos Database
+CREATE TABLE IF NOT EXISTS photos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_id TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
-
-
+-- Comments Database
+CREATE TABLE IF NOT EXISTS comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    comment_text TEXT NOT NULL,
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL
+    created_id TIMESTAMP,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+-- likes databases
+CREATE TABLE IF NOT EXISTS likes (
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
