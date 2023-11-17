0e5ea1bb5e1cac343469dea549d8d1f89a634c0b
Jared Preyer <japr8735@colorado.edu>
2023-11-17 11:48:01 -0700

    updated database

--- project/src/init_data/create.sql
+++ project/src/init_data/create.sql
@@ -3,32 +3,22 @@
 CREATE TABLE IF NOT EXISTS users(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(70) NOT NULL
+    password VARCHAR(70) NOT NULL,
+    image_url VARCHAR(255) NOT NULL
 );
--- Photos Database
-CREATE TABLE IF NOT EXISTS photos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    image_url VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    created_id TIMESTAMP,
-    FOREIGN KEY(user_id) REFERENCES users(id)
-);
+
 -- Comments Database
 CREATE TABLE IF NOT EXISTS comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     comment_text TEXT NOT NULL,
     user_id INT NOT NULL,
-    photo_id INT NOT NULL
     created_id TIMESTAMP,
-    FOREIGN KEY(photo_id) REFERENCES photos(id),
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 -- likes databases
 CREATE TABLE IF NOT EXISTS likes (
     user_id INT NOT NULL,
-    photo_id INT NOT NULL,
     created_at TIMESTAMP,
     FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(photo_id) REFERENCES photos(id),
-    PRIMARY KEY(user_id, photo_id)
+    PRIMARY KEY(user_id)
 );
---
+++ project/src/init_data/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (username) VALUES
+('Paul'),
+('Alexis'),
+('Aaron');
+
+INSERT INTO photos (image_url, user_id) VALUES
+('/images/1.jpg', 2),
+('/images/2.jpg', 3),
+('/images/3.jpg', 1);
+
+INSERT INTO 
