ff22b8efb46feac793fb781755938b734ef6d427
Paul Rodriguez <mildmoose@pm.me>
2023-11-27 19:38:22 -0700

    Menu bar adjustments to show if logged in, finished comic/:id along with add, and delete a review, styling fixes for login/register

--- project/src/init_data/create.sql
+++ project/src/init_data/create.sql
@@ -1,24 +1,43 @@
+-- -- using CREATE TABLE IF NOT EXISTS:l ensures that existing data is not lost due to the table being dropped and recreated
+-- -- For Login in Page and Creating a user
+-- CREATE TABLE IF NOT EXISTS users(
+--     id SERIAL PRIMARY KEY,
+--     username VARCHAR(50) UNIQUE NOT NULL,
+--     password VARCHAR(70) NOT NULL,
+--     image_url VARCHAR(255) NOT NULL
+-- );
+
+-- -- Comments Database
+-- CREATE TABLE IF NOT EXISTS comments (
+--     id SERIAL PRIMARY KEY,
+--     comment_text TEXT NOT NULL,
+--     user_id INT NOT NULL,
+--     created_id TIMESTAMP,
+--     FOREIGN KEY(user_id) REFERENCES users(id)
+-- );
+-- -- likes databases
+-- CREATE TABLE IF NOT EXISTS likes (
+--     user_id INT NOT NULL,
+--     created_at TIMESTAMP,
+--     FOREIGN KEY(user_id) REFERENCES users(id),
+--     PRIMARY KEY(user_id)
+-- );
+
+-- THE ABOVE WAS CAUSING ERRORS
+
 -- using CREATE TABLE IF NOT EXISTS:l ensures that existing data is not lost due to the table being dropped and recreated
 -- For Login in Page and Creating a user
 CREATE TABLE IF NOT EXISTS users(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(70) NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+    username VARCHAR(16) PRIMARY KEY,
+    password VARCHAR(70) NOT NULL
 );
-
--- Comments Database
-CREATE TABLE IF NOT EXISTS comments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    comment_text TEXT NOT NULL,
-    user_id INT NOT NULL,
-    created_id TIMESTAMP,
-    FOREIGN KEY(user_id) REFERENCES users(id)
-);
--- likes databases
-CREATE TABLE IF NOT EXISTS likes (
-    user_id INT NOT NULL,
-    created_at TIMESTAMP,
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    PRIMARY KEY(user_id)
+-- CREATE TABLE IF NOT EXISTS reviews;
+-- For Reviews have users_id with the text, rating, and date
+CREATE TABLE IF NOT EXISTS reviews(
+    id SERIAL PRIMARY KEY,
+    comic_id INT NOT NULL,
+    review VARCHAR(256) NOT NULL,
+    rating INT NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    ts timestamp default current_timestamp
 );
